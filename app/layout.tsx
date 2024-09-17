bd227fc3804f239a2d7c572b8d1d269a29907884
Dmitrii Scarebnii <shkarebnii03@gmail.com>
2024-09-17 18:02:04 +0300

    Dark Mode (Admin)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider, RedirectToSignIn, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 
 
@@ -29,6 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider>
     <html lang="en">
     <body>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <ToasterProvider/>
       < ModalProvider/>
     <header>
@@ -37,6 +39,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </SignedOut>
     </header>
     <main>{children}</main>
+    </ThemeProvider>
     </body>
     </html>
   </ClerkProvider>
